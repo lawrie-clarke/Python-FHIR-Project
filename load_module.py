9664b6210fb0f1bd55cd974ca885e54784e051c5
lawrieclarke <lawrie_clarke@hotmail.co.uk>
2024-07-12 16:24:21 +0100

    Addition of load functions

---
+++ load_module.py
@@ -0,0 +1,24 @@
+from google.cloud import bigquery
+import os
+import extract_module #can remove once tested this works
+
+#TODO Add exception handling
+#TODO Add logging
+
+def load_to_bq(fhir_resource_list,table_id):
+    client = bigquery.Client()                                  # Construct a BigQuery client object.
+    job_config = bigquery.LoadJobConfig(
+        autodetect=True,
+        source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+    )
+    load_job = client.load_table_from_json(                     # Make an API request.
+        json_rows = fhir_resource_list,
+        destination = table_id,
+        location= "US",
+        project = "cosmic-gizmo-269713",
+        job_config=job_config,
+    )                                                   
+    assert load_job.job_type == "load"
+    load_job.result()                                           # Waits for the job to complete.
+    assert load_job.state == "DONE"
+    table = client.get_table(table_id)

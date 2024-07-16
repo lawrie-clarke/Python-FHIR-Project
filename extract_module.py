e9780690e0d407b98a8445465f6ffe498bec0b68
lawrieclarke <lawrie_clarke@hotmail.co.uk>
2024-07-16 15:08:58 +0100

    Improvement to extraction to iterate over all files in zip

--- extract_module.py
+++ extract_module.py
@@ -97,14 +97,10 @@ def import_from_local_file(file_path):
                 continue
     #print(patients)
     return patients, organizations, practitioners, encounters, conditions, medicationrequests, claims, careteams, goals, careplans, explanationofbenefits, observations, procedures, immunizations, diagnostictreports
-
 #import_from_local_file('/home/lawrieclarke/Python-FHIR-Project/Python-FHIR-Project/sample_full.json')
 
-
-#extract from web,open zip and write each file to a list of the relevant fhir resource
-def import_from_web(url,file):
+def import_from_web(url):
     response = requests.get(url)                                        #gets a html response which includes status, headers, body (not for get) and text
-    zip = zipfile.ZipFile(io.BytesIO(response.content))                 #convert to a file like object which zipfile can recognise and then make a zipfile object
     #resources = []  
     patients = []    
     organizations = []
@@ -120,75 +116,82 @@ def import_from_web(url,file):
     observations = []
     procedures = []
     immunizations = []
-    diagnostictreports = []   
-    with zip.open(file) as json_file:                                   #open zip
-        data = json_file.read()                                         #convert to bytes
-        bundle = construct_fhir_element('Bundle', data)                 #construct bundle
-    if bundle.entry is not None:                       
-        for entry in bundle.entry:
-            #resources.append(entry.resource.json())
-            if entry.resource.resource_type == 'Patient':
-                json_string = entry.resource.json()                     #convert to json string
-                json_object = json.loads(json_string)                   #convert to json object
-                patients.append(json_object)                            #add to list to make list of dictionaries (what bq expects)          
-            elif entry.resource.resource_type == 'Organization':
-                json_string = entry.resource.json()                     #convert to json string
-                json_object = json.loads(json_string)                   #convert to json object
-                organizations.append(json_object)                       #add to list to make list of dictionaries (what bq expects)
-            elif entry.resource.resource_type == 'Practitioner':
-                json_string = entry.resource.json()                     #convert to json string
-                json_object = json.loads(json_string)                   #convert to json object
-                practitioners.append(json_object)                       #add to list to make list of dictionaries (what bq expects)
-            elif entry.resource.resource_type == 'Encounter':
-                json_string = entry.resource.json()                     #convert to json string
-                json_object = json.loads(json_string)                   #convert to json object
-                encounters.append(json_object)                          #add to list to make list of dictionaries (what bq expects)
-            elif entry.resource.resource_type == 'Condition':
-                json_string = entry.resource.json()                     #convert to json string
-                json_object = json.loads(json_string)                   #convert to json object
-                conditions.append(json_object)                          #add to list to make list of dictionaries (what bq expects)
-            elif entry.resource.resource_type == 'MedicationRequest':
-                json_string = entry.resource.json()                     #convert to json string
-                json_object = json.loads(json_string)                   #convert to json object
-                medicationrequests.append(json_object)                  #add to list to make list of dictionaries (what bq expects)
-            elif entry.resource.resource_type == 'Claim':
-                json_string = entry.resource.json()                     #convert to json string
-                json_object = json.loads(json_string)                   #convert to json object
-                claims.append(json_object)                              #add to list to make list of dictionaries (what bq expects)
-            elif entry.resource.resource_type == 'CareTeam':
-                json_string = entry.resource.json()                     #convert to json string
-                json_object = json.loads(json_string)                   #convert to json object
-                careteams.append(json_object)                           #add to list to make list of dictionaries (what bq expects)
-            elif entry.resource.resource_type == 'Goal':
-                json_string = entry.resource.json()                     #convert to json string
-                json_object = json.loads(json_string)                   #convert to json object
-                goals.append(json_object)                               #add to list to make list of dictionaries (what bq expects)
-            elif entry.resource.resource_type == 'CarePlan':
-                json_string = entry.resource.json()                     #convert to json string
-                json_object = json.loads(json_string)                   #convert to json object
-                careplans.append(json_object)                           #add to list to make list of dictionaries (what bq expects)
-            elif entry.resource.resource_type == 'ExplanationOfBenefit':
-                json_string = entry.resource.json()                     #convert to json string
-                json_object = json.loads(json_string)                   #convert to json object
-                explanationofbenefits.append(json_object)               #add to list to make list of dictionaries (what bq expects)
-            elif entry.resource.resource_type == 'Observation':
-                json_string = entry.resource.json()                     #convert to json string
-                json_object = json.loads(json_string)                   #convert to json object
-                observations.append(json_object)                        #add to list to make list of dictionaries (what bq expects)
-            elif entry.resource.resource_type == 'Procedure':
-                json_string = entry.resource.json()                     #convert to json string
-                json_object = json.loads(json_string)                   #convert to json object
-                procedures.append(json_object)                          #add to list to make list of dictionaries (what bq expects)
-            elif entry.resource.resource_type == 'Immunization':
-                json_string = entry.resource.json()                     #convert to json string
-                json_object = json.loads(json_string)                   #convert to json object
-                immunizations.append(json_object)                       #add to list to make list of dictionaries (what bq expects)
-            elif entry.resource.resource_type == 'DiagnosticReport':
-                json_string = entry.resource.json()                     #convert to json string
-                json_object = json.loads(json_string)                   #convert to json object
-                diagnostictreports.append(json_object)                  #add to list to make list of dictionaries (what bq expects)
-            else:
-                continue
+    diagnostictreports = []  
+    #TODO look at vectorising the nested for loop to improve performance
+    with zipfile.ZipFile(io.BytesIO(response.content),"r") as zip:                  #convert to a file like object which zipfile can recognise and then make a zipfile object
+        for member in zip.infolist():                                               #iterate over contents of zip file
+            if not member.is_dir():                                                 #ignore the directories and just iterate over files
+                with zip.open(member) as json_file:                                 #open the json files
+                    data = json_file.read()                                         #convert to bytes 
+                    try:
+                        bundle = construct_fhir_element('Bundle', data)             #construct bundle
+                    except ValueError:
+                        print(json_file.name + " failed because of a ValueError")   #flag value error if issue with the contents
+                if bundle.entry is not None:                       
+                    for entry in bundle.entry:
+                        #resources.append(entry.resource.json())
+                        if entry.resource.resource_type == 'Patient':
+                            json_string = entry.resource.json()                     #convert to json string
+                            json_object = json.loads(json_string)                   #convert to json object
+                            patients.append(json_object)                            #add to list to make list of dictionaries (what bq expects)          
+                        elif entry.resource.resource_type == 'Organization':
+                            json_string = entry.resource.json()                     #convert to json string
+                            json_object = json.loads(json_string)                   #convert to json object
+                            organizations.append(json_object)                       #add to list to make list of dictionaries (what bq expects)
+                        elif entry.resource.resource_type == 'Practitioner':
+                            json_string = entry.resource.json()                     #convert to json string
+                            json_object = json.loads(json_string)                   #convert to json object
+                            practitioners.append(json_object)                       #add to list to make list of dictionaries (what bq expects)
+                        elif entry.resource.resource_type == 'Encounter':
+                            json_string = entry.resource.json()                     #convert to json string
+                            json_object = json.loads(json_string)                   #convert to json object
+                            encounters.append(json_object)                          #add to list to make list of dictionaries (what bq expects)
+                        elif entry.resource.resource_type == 'Condition':
+                            json_string = entry.resource.json()                     #convert to json string
+                            json_object = json.loads(json_string)                   #convert to json object
+                            conditions.append(json_object)                          #add to list to make list of dictionaries (what bq expects)
+                        elif entry.resource.resource_type == 'MedicationRequest':
+                            json_string = entry.resource.json()                     #convert to json string
+                            json_object = json.loads(json_string)                   #convert to json object
+                            medicationrequests.append(json_object)                  #add to list to make list of dictionaries (what bq expects)
+                        elif entry.resource.resource_type == 'Claim':
+                            json_string = entry.resource.json()                     #convert to json string
+                            json_object = json.loads(json_string)                   #convert to json object
+                            claims.append(json_object)                              #add to list to make list of dictionaries (what bq expects)
+                        elif entry.resource.resource_type == 'CareTeam':
+                            json_string = entry.resource.json()                     #convert to json string
+                            json_object = json.loads(json_string)                   #convert to json object
+                            careteams.append(json_object)                           #add to list to make list of dictionaries (what bq expects)
+                        elif entry.resource.resource_type == 'Goal':
+                            json_string = entry.resource.json()                     #convert to json string
+                            json_object = json.loads(json_string)                   #convert to json object
+                            goals.append(json_object)                               #add to list to make list of dictionaries (what bq expects)
+                        elif entry.resource.resource_type == 'CarePlan':
+                            json_string = entry.resource.json()                     #convert to json string
+                            json_object = json.loads(json_string)                   #convert to json object
+                            careplans.append(json_object)                           #add to list to make list of dictionaries (what bq expects)
+                        elif entry.resource.resource_type == 'ExplanationOfBenefit':
+                            json_string = entry.resource.json()                     #convert to json string
+                            json_object = json.loads(json_string)                   #convert to json object
+                            explanationofbenefits.append(json_object)               #add to list to make list of dictionaries (what bq expects)
+                        elif entry.resource.resource_type == 'Observation':
+                            json_string = entry.resource.json()                     #convert to json string
+                            json_object = json.loads(json_string)                   #convert to json object
+                            observations.append(json_object)                        #add to list to make list of dictionaries (what bq expects)
+                        elif entry.resource.resource_type == 'Procedure':
+                            json_string = entry.resource.json()                     #convert to json string
+                            json_object = json.loads(json_string)                   #convert to json object
+                            procedures.append(json_object)                          #add to list to make list of dictionaries (what bq expects)
+                        elif entry.resource.resource_type == 'Immunization':
+                            json_string = entry.resource.json()                     #convert to json string
+                            json_object = json.loads(json_string)                   #convert to json object
+                            immunizations.append(json_object)                       #add to list to make list of dictionaries (what bq expects)
+                        elif entry.resource.resource_type == 'DiagnosticReport':
+                            json_string = entry.resource.json()                     #convert to json string
+                            json_object = json.loads(json_string)                   #convert to json object
+                            diagnostictreports.append(json_object)                  #add to list to make list of dictionaries (what bq expects)
+                        else:
+                            continue
     #print(patients)
     return patients, organizations, practitioners, encounters, conditions, medicationrequests, claims, careteams, goals, careplans, explanationofbenefits, observations, procedures, immunizations, diagnostictreports  
-#import_from_web("https://synthetichealth.github.io/synthea-sample-data/downloads/synthea_sample_data_fhir_r4_sep2019.zip",'fhir/Willian804_Moen819_439b24b4-6f25-4093-b101-47a39bd061ca.json')
+#import_from_web("https://synthetichealth.github.io/synthea-sample-data/downloads/synthea_sample_data_fhir_r4_sep2019.zip")
